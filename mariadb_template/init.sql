ac9ca20372db7cfb5eb7e1ebc6afb21993afcf81
yuto_kizawa <yuto17kz@gmail.com>
2024-01-08 00:35:33 +0900

    adsf

---
+++ mariadb_template/init.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE IF NOT EXISTS example;
+-- USE example;
+-- 
+-- CREATE TABLE IF NOT EXISTS example_table (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     temperature FLOAT,
+--     co2_level FLOAT
+-- );
+
