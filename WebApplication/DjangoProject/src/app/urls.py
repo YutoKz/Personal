e6b9f0ec5e1416b1bf2bad5afb8ba12daa6f4512
yuto_kizawa <yuto17kz@gmail.com>
2024-01-10 19:31:41 +0900

    ようやくenvwatcherいった

--- WebApplication/DjangoProject/src/app/urls.py
+++ WebApplication/DjangoProject/src/app/urls.py
@@ -1,7 +1,8 @@
-"""app URL Configuration
+"""
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mytodo/', include('mytodo.urls',namespace='mytodo')),
+    path('environment/', include('env_watcher.urls')),
 ]
---
+++ WebApplication/DjangoProject_before/src/env_watcher/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from .views import index
+
+app_name = 'env_watcher'
+
+urlpatterns = [
+    path('', index, name='env-watcher'),
+]
